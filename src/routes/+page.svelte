e9a9a0dbc999c0454aaed0a831a2017bef877a5d
ll736 <ll736@cornell.edu>
2023-03-02 18:49:39 -0500

    Created pocketbase and some basic feature button

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { currentUser } from '$lib/pocketbase';
+</script>
+
+{#if $currentUser}
+	<div class="flex justify-center">
+		<!-- The button to open modal -->
+		<label for="my-modal-4" class="btn btn-primary">Create Post</label>
+
+		<!-- Put this part before </body> tag -->
+		<input type="checkbox" id="my-modal-4" class="modal-toggle" />
+		<label for="my-modal-4" class="modal cursor-pointer">
+			<label class="modal-box relative" for="">
+				<h3 class="text-lg font-bold">Create Post</h3>
+			</label>
+		</label>
+	</div>
+{/if}
